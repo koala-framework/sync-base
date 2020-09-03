4cecffd6eb803fc7cff4b6943cd5e70d6fb163bd
David Neumaier <david.neumaier@vivid-planet.com>
2020-09-03 16:26:37 +0200

    add comments and change signature

--- Services/Sync/LoggerInterface.php
+++ Services/Sync/LoggerInterface.php
@@ -1,11 +1,42 @@
 <?php
 namespace Kwf\SyncBaseBundle\Services\Sync;
 
+use Kwf\SyncBaseBundle\Services\Sync\SyncModel\BasicSyncModel;
+
 interface LoggerInterface
 {
+    /**
+     * Called when the sync starts, before anything else is.
+     * @return void
+     */
     function startSync();
+
+    /**
+     * Sets, if a countable iterator is given, its count.
+     * @param integer $count
+     * @return void
+     */
     function setItemCount($count);
+
+    /**
+     * Is being called for every syncModel that was provided for this sync.
+     * @param $rawData array
+     * @param $index integer
+     * @param $syncModel BasicSyncModel
+     * @return void
+     */
     function processItem($rawData, $index, $syncModel = null);
+
+    /**
+     * @param integer $countItems
+     * @param BasicSyncModel $syncModel
+     * @return void
+     */
     function commitTransaction($countItems, $syncModel = null);
+
+    /**
+     * Is called at the very end of the sync, after everything else has finished.
+     * @return void
+     */
     function finishedSync();
 }
