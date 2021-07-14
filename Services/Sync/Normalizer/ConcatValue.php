22d4cedbdfcc567930613ed28dafa465a0261db0
Johannes Huber <jh@vivid-planet.com>
2021-07-14 20:39:20 +0200

    Add option to specify separator for concat-normalizer

--- Services/Sync/Normalizer/ConcatValue.php
+++ Services/Sync/Normalizer/ConcatValue.php
@@ -5,8 +5,15 @@ use Kwf\SyncBaseBundle\Services\Sync\NormalizerInterface;
 
 class ConcatValue implements NormalizerInterface
 {
+    protected $_separator;
+
+    public function __construct($separator = '')
+    {
+        $this->_separator = $separator;
+    }
+
     public function normalize($value)
     {
-        return is_array($value) ? implode($value) : $value;
+        return is_array($value) ? implode($this->_separator, $value) : $value;
     }
 }
